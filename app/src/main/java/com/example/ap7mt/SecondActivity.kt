9c284c1fdc34f02960060fec8338541d1834a0cf
JohnyTes <j3_horak@utb.cz>
2023-12-19 21:30:33 +0100

    Shared Preferences

--- app/src/main/java/com/example/ap7mt/SecondActivity.kt
+++ app/src/main/java/com/example/ap7mt/SecondActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ap7mt
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -15,23 +16,49 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
         val passedData = intent.getIntExtra("passedData",1)
+
+        val sharedPref=getSharedPreferences("$passedData", Context.MODE_PRIVATE)
+        val editor=sharedPref.edit()
+
         val textview_name=findViewById<TextView>(R.id.textViewName)
         val textview_origin=findViewById<TextView>(R.id.textViewOrigin)
         val textview_species=findViewById<TextView>(R.id.textViewSpecies)
         val textview_status=findViewById<TextView>(R.id.textViewStatus)
         val imageView1=findViewById<ImageView>(R.id.imageView)
-        viewModel.refreshCharacter(passedData)
-        viewModel.characterByIdLiveData.observe(this){response->
-            if (response==null){
-                return@observe
-            }
-            val name=response.name
-            Picasso.get().load(response.image).into(imageView1)
+
+        val name=sharedPref.getString("name",null)
+
+        if (name!=null){
+            val status=sharedPref.getString("status",null)
+            val origin=sharedPref.getString("origin",null)
+            val species=sharedPref.getString("species",null)
             textview_name.text=name
-            textview_origin.text=response.origin.name
-            textview_species.text=response.species
-            textview_status.text=response.status
+            textview_origin.text=origin
+            textview_species.text=species
+            textview_status.text=status
+        }else{
+
+            viewModel.refreshCharacter(passedData)
+            viewModel.characterByIdLiveData.observe(this){response->
+                if (response==null){
+                    return@observe
+                }
+                val name=response.name
+                Picasso.get().load(response.image).into(imageView1)
+                textview_name.text=name
+                textview_origin.text=response.origin.name
+                textview_species.text=response.species
+                textview_status.text=response.status
+                editor.apply{
+                    putString("name",response.name)
+                    putString("origin",response.origin.name)
+                    putString("species",response.species)
+                    putString("status",response.status)
+                    apply()
+                }
+            }
         }
     }
 }
